f242e3baef5fb31e8d2fa7bf827ad108beb507dc
mogamatyaseenkannemeyer <yaseenkannemeyer@gmail.com>
2025-12-03 15:47:42 +0200

    Add Documentation component and update App to include it

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Documentation from './Documentation';
 
 function App() {
+  const headerText = "Welcome to My React App";
   return (
     <div className="App">
       <header className="App-header">
+        <Documentation />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {headerText}
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+    
       </header>
     </div>
   );
---
+++ src/Documentation.jsx
@@ -0,0 +1,16 @@
+import react from 'react';
+
+function Documentation() {
+
+  return (
+   <div>
+    <h1>
+        This is our new component.
+    </h1>
+    <button>this is a button</button>
+   </div>
+ 
+  );
+}
+
+export default  Documentation;
